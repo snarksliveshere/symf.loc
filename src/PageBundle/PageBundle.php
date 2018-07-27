5f18f7562df5bdeb3f45d2d7c7862a55696569b0
snarksliveshere <snarksliveshere@gmail.com>
2018-07-27 23:02:08 +0300

    initial commit

---
+++ src/PageBundle/PageBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: -
+ * Date: 27.07.2018
+ * Time: 22:59
+ */
+
+namespace PageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PageBundle extends Bundle
+{
+
+}
---
+++ src/TermBundle/TermBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: -
+ * Date: 27.07.2018
+ * Time: 23:01
+ */
+
+namespace TermBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TermBundle extends Bundle
+{
+
+}
